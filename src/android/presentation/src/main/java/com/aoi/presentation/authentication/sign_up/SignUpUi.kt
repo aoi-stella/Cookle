fd9546a4fb0769ec43e3ac1af85081d48dec4f1f
aoi-stella <aoi.stella.dev@gmail.com>
2024-05-11 23:42:43 +0900

    feature: サインアップ画面のUI及びViewModelの初期ファイルを追加した

---
+++ src/android/presentation/src/main/java/com/aoi/presentation/authentication/sign_up/SignUpUi.kt
@@ -0,0 +1,11 @@
+package com.aoi.presentation.authentication.sign_up
+
+import androidx.compose.runtime.Composable
+
+/**
+ * サインアップ画面のUI
+ * @param onNavigate サインイン画面から他の画面に遷移するためのコールバック
+ */
+@Composable
+fun SignUpScreen(onNavigate: () -> Unit) {
+}
---
+++ src/android/presentation/src/main/java/com/aoi/presentation/authentication/sign_up/ViewModel.kt
@@ -0,0 +1,9 @@
+package com.aoi.presentation.authentication.sign_up
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * サインアップ画面のViewModel
+ */
+class SignUpViewModel: ViewModel() {
+}
